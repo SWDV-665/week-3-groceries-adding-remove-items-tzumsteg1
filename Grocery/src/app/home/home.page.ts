e9cdd77e226f23010ee0f6d133cacbbd799888bd
tzumsteg1 <tzumsteg1@live.maryville.edu>
2019-07-18 20:02:20 -0500

    add capabilities with AlertController

--- Grocery/src/app/home/home.page.ts
+++ Grocery/src/app/home/home.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import { ToastController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+import { NavController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -30,7 +30,7 @@ export class HomePage {
     }
   ];
 
-  constructor() {
+  constructor(public toastCtrl: ToastController, public alertCtrl: AlertController) {
 
   }
   removeItem(item){
@@ -40,5 +40,42 @@ export class HomePage {
       duration: 3000
     })
     toast.present();
+  }
+
+  addItem(){
+    console.log("Adding Item");
+
+  }
+  showAddItemPrompt(){
+    const promp = this.alertCtrl.creat({
+      title: "Add Item",
+      message: "Please enter your item.",
+      inputs: [
+        {
+        name: 'name',
+        placeholder: 'Name'
+        },
+        {
+          name: 'quantity',
+          placeholder: 'Quantity'
+          },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: item=> {
+            console.log('Saved clicked', item);
+            this.items.push(item);
+          }
+        }
+      ]
+    });
+    prompt.present();
   }
 }
