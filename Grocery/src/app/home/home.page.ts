775fb85b836bc5de78ca1e1da1a0b85784185b3e
tzumsteg1 <tzumsteg1@live.maryville.edu>
2019-07-18 19:12:39 -0500

    added item syntax

--- Grocery/src/app/home/home.page.ts
+++ Grocery/src/app/home/home.page.ts
@@ -7,6 +7,27 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  title = "Grocery List";
+
+  items = [
+    {
+      name: "Milk",
+      quantity: 2
+    },
+    {
+      name: "Bread",
+      quantity: 1
+    },
+    {
+      name: "Banana",
+      quantity: 6
+    },
+    {
+      name: "Eggs",
+      quantity: 1
+    }
+  ];
+
   constructor() {}
 
 }
